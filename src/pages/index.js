59df53d237ddc675de49da2769fabebcc7986844
min050410 <min050410@gmail.com>
2021-11-24 10:18:11 +0900

    add: mdx apply, syntax highlighting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,42 @@
 //step 1 : Import react
 import * as React from "react";
 import { Link } from 'gatsby';
-import { MDXRenderer } from "gatsby-plugin-mdx"
+import Post from './example.mdx'
+import Syntax from './syntax.mdx'
 import { MDXProvider } from "@mdx-js/react"
-// import Content from '.youngmin/src/pages/example.mdx'
-// import PageLayout from './src/md/example';
+import codeblock from "./codeblock";
+import Highlight, {defaultProps} from 'prism-react-renderer'
+// import {compile} from '@mdx-js/mdx'
+// import rehypeHighlight from 'rehype-highlight'
+
 
 //step 2: define your component 
 const IndexPage = () => {
   var hi = 0;
-  
+
+  const components = { //코드 스타일링
+    code: codeblock,
+  };
+
   return(
     <main>
       <title>Home Page${hi}</title>
       <h1>Welcome to my Gatsby site!</h1>
       <Link to="/about">About</Link>
       <p>I'm making thist by following gatsby tuto</p>
+      <MDXProvider components={components}>
+        <Syntax/>
+      </MDXProvider>
+      {/* <Post components={{code}}/> */}
       
     </main>
+    
   )
 }
 //step3 export 
 export default IndexPage; 
+
+// console.log(Post.title);
+
 
 
