b0c8364e02731b012160c706417815f045922cf2
min050410 <min050410@gmail.com>
2021-11-28 13:02:39 +0900

    sort: file cleanup

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import Layout from '../components/layout';
-
 
 const AboutPage = () => {
   return (
-    <Layout pageTitle="About Me">
         <p>Hi there 👋</p>
-    </Layout>
   )
 }
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,30 @@
-//step 1 : Import react
 import * as React from "react";
 import { Link } from 'gatsby';
-import styled from "styled-components";
-import Post from './example.mdx'
-import PostStyle from '../components/styles/postStyle';
-// import Syntax from './syntax.mdx'
+import Post from '../md/example.mdx'
 import { MDXProvider } from "@mdx-js/react"
-import CodeBlock from "./codeblock";
+import CodeBlock from "./components/codeblock";
+import './styles/index.sass';
 
-//step 2: define your component
-const PostContentSection = styled.section`
-  margin-top: 60px;
-  ${PostStyle};
-`;
 // @font-face {
 //   font-family: "NanumSquareR";
 //   src: url("../assets/font/NanumSquareR.ttf") format("truetype");
 //   }
 //   font-family: 'NanumSquareR';
 
-
 const IndexPage = () => {
-  var hi = 0;
   const components = { //코드 스타일링
     code: CodeBlock,
   };
 
   return(
     <main>  
-      <title>Home Page${hi}</title>
-      <h1>Welcome to my Gatsby site!</h1>
+      <title>개발중입니다..</title>
+      <h1>개발중입니다...</h1>
       <Link to="/about">About</Link>
       <p>I'm making thist by following gatsby tuto</p>
-      <PostContentSection>
       <MDXProvider components={components} >
         <Post/>
       </MDXProvider>
-      </PostContentSection>
     </main>
   )
 }
