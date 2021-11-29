6f611d6cd6f584ff393fe56feb891469c1976315
youngmin <min050410@gmail.com>
2021-11-29 22:48:18 +0900

    add: post list로 나누기

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,27 @@
 import * as React from "react";
 import { Link } from 'gatsby';
-// import("./math").then(math => {
-//   console.log(math.add(16, 26));
-// });
 import posts from '../md/indexpost'
 import Post from '../md/example.mdx'
 import { MDXProvider } from "@mdx-js/react"
 import CodeBlock from "./components/codeblock";
 import './styles/index.sass';
 
+
 const IndexPage = () => {
-  const postlist = posts.map((post)=>(
+
+  const postlist = posts.map((post)=>
+  (
+    <Link to="postitem">
     <div className="card">
-    <h3>{post.title}</h3>
+    <h3>{post.title}</h3> 
     <img src={require(`../assets/images/${post.img}.png`).default} width="200px"/>
     <span>{post.tag}</span>
     <span>{post.tag2}</span>
     <span>{post.tag3}</span>
     </div>
+    </Link>
   ));
-  
-  
+
   const components = { //코드 스타일링
     code: CodeBlock,
   };
@@ -31,7 +32,6 @@ const IndexPage = () => {
       <title>개발중입니다..</title>
       {postlist}
       <h2>개발중입니다...</h2>
-      <Link to="/about">소개</Link>
       <MDXProvider components={components} >
         <Post/>
       </MDXProvider>
---
+++ src/pages/postitem.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+const Postimport = (prop) => {
+    return(
+    <h3>안녕하세요</h3>
+    )
+}
+export default Postimport;
