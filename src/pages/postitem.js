ebfd9665379e5d70344475318b47493682d6e55d
youngmin <min050410@gmail.com>
2021-11-30 09:12:56 +0900

    hotfix: 예외 처리

--- src/pages/postitem.js
+++ src/pages/postitem.js
@@ -2,20 +2,25 @@ import * as React from "react";
 import { MDXProvider } from "@mdx-js/react"
 import CodeBlock from "./components/codeblock";
 import './styles/index.sass';
+import NotFoundPage from "./404" 
 // import Post from '../md/example.mdx'
 
 const Postimport = ({ location }) => {
     // const item = require(`../md/${location.state.fromFeed}`)
-    const Postitem = require(`../md/${location.state.fromFeed}.mdx`).default
-
-    const components = { //코드 스타일링
-        code: CodeBlock,
-    };
-    
-    return(
-    <MDXProvider components={components} >
-    <Postitem/>
-    </MDXProvider>
-    )
+    if(location.state.fromFeed){
+        const Postitem = require(`../md/${location.state.fromFeed}.mdx`).default
+        const components = { //코드 스타일링
+            code: CodeBlock,
+        };
+        
+        return(
+        <MDXProvider components={components} >
+        <Postitem/>
+        </MDXProvider>
+        )
+    }
+    else{
+        return(<NotFoundPage/>)
+    }
 }
 export default Postimport;  
