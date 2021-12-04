056b34d60dea1b20eec8c070ad15a82a5dadcb43
youngmin <min050410@gmail.com>
2021-12-05 00:00:27 +0900

    hotfix toString함수 변경

--- src/pages/postitem.js
+++ src/pages/postitem.js
@@ -3,20 +3,19 @@ import { MDXProvider } from "@mdx-js/react"
 import CodeBlock from "./components/codeblock";
 import './styles/index.sass';
 import NotFoundPage from "./404" 
-// import Post from '../md/example.mdx'
+
 
 const Postimport = ({ location }) => {
-    const product = String(location.state.fromFeed)
-    if(location.state.fromFeed){
-        const Postitem = require(`../md/${product}.mdx`).default
-        const components = { //코드 스타일링
-            code: CodeBlock,
-        };
-        return(
-            <MDXProvider components={components} >
-            <Postitem/>
-            </MDXProvider>
-        )
-    }
+    const product = location.state.fromFeed.toString()
+    const Postitem = require(`../md/${product.toString()}.mdx`).default
+    const components = { //코드 스타일링
+        code: CodeBlock,
+    };
+    return(
+        <MDXProvider components={components} >
+        <Postitem/>
+        </MDXProvider>
+    )
 }
+
 export default Postimport;  
