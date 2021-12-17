ec4ebfdc29caaa2fe1292719b067443f8b2e03e2
youngmin <min050410@gmail.com>
2021-12-17 13:06:44 +0900

    add: header complete

--- src/pages/components/codeblock.js
+++ src/pages/components/codeblock.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Highlight, {defaultProps} from 'prism-react-renderer'
-import theme from 'prism-react-renderer/themes/github';
+import theme from 'prism-react-renderer/themes/nightOwl';
 
 const Pre = styled.pre`
   text-align: left;
