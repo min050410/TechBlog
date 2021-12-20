65e885d6e0ab3a001a6a55205961588651985202
youngmin <min050410@gmail.com>
2021-12-20 18:41:08 +0900

    del: robot meta

--- src/pages/postitem.tsx
+++ src/pages/postitem.tsx
@@ -32,7 +32,6 @@ const Postimport = ({ location }) => {
                 <Helmet>
                     <title>Dev Log | {filename}</title>
                     <meta name="google-site-verification" content="Vfqlx3gjgzF7VwfWKG3BDziWEL76_QpnF4LvF0bgj8I" />
-                    <meta name="robots" content="noindex"></meta>
                     <meta name="description" content={`Dev Log | ${filename} - 고등학교 1학년 재학생이 만든 코딩과 관련된 갖가지 정보들과 에러 해결 방법 등을 모아놓은 블로그입니다.`}></meta>
                 </Helmet>
                 <Header />
