f5488b4d4215258db6d0bc4c8f84b7f81f94e81f
youngmin <min050410@gmail.com>
2022-01-08 22:32:29 +0900

    fix: location is not defined

--- src/components/recent.tsx
+++ src/components/recent.tsx
@@ -9,65 +9,66 @@ import recent from '../md/recent'
 import '../styles/recent.sass'
 
 const Recent_components = () => {
-
-  const params = new URLSearchParams(location.search);
-
-  const [filter_key, setFilter_key] = useState("");
-
-  const so = () => {
-    //useEffect용 비동기를 위한 함수
+  if (location.search === undefined) {
+    return null;
   }
-
-  if (location.search) {
-    so()
+  else if (location.search == null) {
+    return null;
   }
+  else {
+    const [filter_key, setFilter_key] = useState("");
+    const so = () => {
+      //useEffect용 비동기를 위한 함수
+    }
+    so();
+    const params = new URLSearchParams(location.search);
+    useEffect(() => {
+      setFilter_key(params.get("f"))
+    }, [so])
 
-  useEffect(() => {
-    setFilter_key(params.get("f"))
-  }, [so])
+    //filter함수를 통한 필터링
+    const recent_list = recent.filter(word => word.filter == filter_key || filter_key == null).map((post) =>
+    (
+      <div className="recent_content">
+        <Link to={`postitem/?name=${post.filename}`}>
+          <div className="post">
+            <div className="left">
+              {/* <Link to="postitem/" state={{ myProp: post.filename }}> */}
+              <div className="imgbox">
+                <img
+                  src={require(`../assets/images/${post.img}.png`).default}
+                  alt={post.img}
+                />
+              </div>
+              <div id="text_group">
+                <h3>{post.title}</h3>
+                <div>{post.date}</div>
+              </div>
+              {/* star */}
+              {/* <img className="copylink" src="https://user-images.githubusercontent.com/45661217/146631169-54b842c2-d174-488b-9192-02780e57b2a5.png"></img> */}
+            </div>
+            <div className="tag">
+              {(post.tag) ? <span>{post.tag}</span> : null}
+              {(post.tag2) ? <span>{post.tag2}</span> : null}
+              {(post.tag3) ? <span>{post.tag3}</span> : null}
+            </div>
+          </div>
+        </Link>
+      </div>
+    ));
 
-  //filter함수를 통한 필터링
-  const recent_list = recent.filter(word => word.filter == filter_key || filter_key == null).map((post) =>
-  (
-    <div className="recent_content">
-      <Link to={`postitem/?name=${post.filename}`}>
-        <div className="post">
-          <div className="left">
-            {/* <Link to="postitem/" state={{ myProp: post.filename }}> */}
-            <div className="imgbox">
-              <img
-                src={require(`../assets/images/${post.img}.png`).default}
-                alt={post.img}
-              />
-            </div>
-            <div id="text_group">
-              <h3>{post.title}</h3>
-              <div>{post.date}</div>
-            </div>
-            {/* star */}
-            {/* <img className="copylink" src="https://user-images.githubusercontent.com/45661217/146631169-54b842c2-d174-488b-9192-02780e57b2a5.png"></img> */}
-          </div>
-          <div className="tag">
-            {(post.tag) ? <span>{post.tag}</span> : null}
-            {(post.tag2) ? <span>{post.tag2}</span> : null}
-            {(post.tag3) ? <span>{post.tag3}</span> : null}
-          </div>
+    return (
+      <section>
+        <div className="header_wrap">
+          <div className="header">최근 블로그</div>
+          {filter_key ? <span className="filter">✨{filter_key} 필터 적용됨</span> : null}
         </div>
-      </Link>
-    </div>
-  ));
-
-  return (
-    <section>
-      <div className="header_wrap">
-      <div className="header">최근 블로그</div>
-      {filter_key ? <span className="filter">✨{filter_key} 필터 적용됨</span> : null}
-      </div>
-      <div className="horizontal_container">
-        {recent_list}
-      </div>
-    </section>
-  )
+        <div className="horizontal_container">
+          {recent_list}
+        </div>
+      </section>
+    )
+  }
 }
 
 export default Recent_components;
