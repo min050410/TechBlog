5610424ee9e1b2b4887f22af2b105299ae705352
youngmin <min050410@gmail.com>
2022-12-26 10:00:25 +0900

    fix: gatsby location 예외처리

--- src/hooks/useSearchParam.ts
+++ src/hooks/useSearchParam.ts
@@ -2,15 +2,15 @@ import React from 'react';
 
 export const useSearchParam = (param: string) => {
 
-    const getValue = React.useCallback(() => 
-        new URLSearchParams(location.search).get(param)
-    , [param]);
+    const getValue = React.useCallback(() => {
+        if (typeof location === "undefined") return;
+        return new URLSearchParams(location?.search).get(param)
+    }, [param]);
 
-    const [value, setValue] = React.useState(getValue);
-
+    const [value, setValue] = React.useState<string | null | undefined>(getValue);
     React.useEffect(() => {
         setValue(getValue());
-    }, [location.search]);
+    }, [location?.search]);
 
     return value;
 };
