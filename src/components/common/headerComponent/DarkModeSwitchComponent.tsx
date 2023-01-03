48e6b286cefd892b70882cc97f88c0af05887062
youngmin <min050410@gmail.com>
2023-01-03 21:03:11 +0900

    fix: 스위치 리랜더링 방식 변경

--- src/components/common/headerComponent/DarkModeSwitchComponent.tsx
+++ src/components/common/headerComponent/DarkModeSwitchComponent.tsx
@@ -1,3 +1,4 @@
+import { JSXElement } from "estree-jsx";
 import * as React from "react";
 import { useRecoilState } from "recoil";
 import { themeColorState } from "../../../recoil/theme/theme";
@@ -12,20 +13,19 @@ const DarkModeSwichComponent = () => {
         setColorMode(colorMode === 'dark' ? 'light' : 'dark');
     }
 
+    const [SwitchEl, setSwitchEl] = React.useState<JSX.Element>()
     React.useEffect(() => {
         document.documentElement.setAttribute('theme', colorMode);
         window.localStorage.setItem('color-mode', colorMode);
         document.documentElement.removeAttribute('style');
+        setSwitchEl(
+            <div
+                className={colorMode === 'dark' ? 'mod-dark' : 'mod-white'}
+                onClick={darkModeHandling}>
+                <div className="circle"></div>
+            </div>
+        )
     }, [colorMode])
-
-    // colorMode가 바뀌었을 때 rendering
-    const SwitchEl = React.useMemo(() => 
-        <div
-            className={colorMode === 'dark' ? 'mod-dark' : 'mod-white'}
-            onClick={darkModeHandling}>
-            <div className="circle"></div>
-        </div>
-    , [colorMode])
 
     return (
         <div>
