0705f16740f2c8209398c85079b566aa92aa1bb2
min050410 <min050410@gmail.com>
2023-03-31 23:52:30 +0900

    refac: 인기 있는 블로그 리스트 컴포넌트 분리

--- src/components/layout/popularComponent/PopularComponent.tsx
+++ src/components/layout/popularComponent/PopularComponent.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Link } from "gatsby";
 import popularPostsData from "./popularPostsData";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -8,47 +7,33 @@ import "slick-carousel/slick/slick-theme.css";
 export const slickSettings = {
     dots: true,
     arrows: false,
-    // autoplay: true,
     infinite: true,
     speed: 1000,
     autoplaySpeed: 5000,
     slidesToShow: 6,
     slidesToScroll: 6,
-    // variableWidth: true,
 };
 
 //styles
 import "../../../styles/card.sass";
+import PopularCardListComponent from "./PopularCardListComponent";
 
 const PopularComponent: React.FC = () => {
-    const popular_list = popularPostsData.map((post, i) => (
-        <div className="card" key={i}>
-            <div className="imgbox">
-                <Link to={`postitem/?name=${post.filename}`}>
-                    <img
-                        src={
-                            require(`../../../../static/gradients/${post.id}.png`)
-                                .default
-                        }
-                        alt={post.imgLineTwo}
-                    />
-                    <div className="recent-img-text-one">
-                        <p>{post.imgLineOne}</p>
-                    </div>
-                    <div className="recent-img-text-two">
-                        <p>{post.imgLineTwo}</p>
-                    </div>
-                </Link>
-            </div>
-            <h3>{post.title}</h3>
-        </div>
-    ));
-
     return (
         <section>
             <div className="header">인기있는 블로그</div>
             <div className="slider-item">
-                <Slider {...slickSettings}>{popular_list}</Slider>
+                <Slider {...slickSettings}>
+                    {popularPostsData.map((popularPost) => (
+                        <PopularCardListComponent
+                            id={popularPost.id}
+                            filename={popularPost.filename}
+                            imgLineOne={popularPost.imgLineOne}
+                            imgLineTwo={popularPost.imgLineTwo}
+                            title={popularPost.title}
+                        />
+                    ))}
+                </Slider>
             </div>
         </section>
     );
