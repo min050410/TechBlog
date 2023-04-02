981587e99ef48d65383db230bd1df063fa431210
min050410 <min050410@gmail.com>
2023-04-03 00:23:59 +0900

    fix: key is not a prop

--- src/components/layout/portfolioComponent/ProjectCardComponent.tsx
+++ src/components/layout/portfolioComponent/ProjectCardComponent.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Link } from "gatsby";
 
 type ProjectCardComponentType = {
-    key: number;
     filename: string;
     stack: string;
     name: string;
@@ -14,7 +13,6 @@ type ProjectCardComponentType = {
 };
 
 const ProjectCardComponent = ({
-    key,
     filename,
     stack,
     name,
@@ -22,11 +20,7 @@ const ProjectCardComponent = ({
     logo,
 }: ProjectCardComponentType) => {
     return (
-        <Link
-            to={`/postitem/?name=${filename}`}
-            className="project--list-card"
-            key={key}
-        >
+        <Link to={`/postitem/?name=${filename}`} className="project--list-card">
             {/* <div className="project--list-card" key={key}> */}
             <div className="project--list-card--stack">{stack}</div>
             <div className="project--list-card--name">{name}</div>
