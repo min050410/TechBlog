3c7a3e660aafd19984f271c87d77f012164477cf
min050410 <min050410@gmail.com>
2023-04-05 09:08:46 +0900

    docs: 오타 수정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
     siteMetadata: {
         title: `Devlog`,
-        siteUrl: `https://www.yourdomain.tld`,
+        siteUrl: `https://devlog.kro.kr`,
     },
     // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
     // If you use VSCode you can also use the GraphQL plugin
